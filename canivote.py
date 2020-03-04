373ba9fdb76c8259eda9a16b74aa4ca1e9d65276
sayaanalam <31066332+sayaanalam@users.noreply.github.com>
2020-03-04 13:55:51 +0530

    Create canivote.py

---
+++ canivote.py
@@ -0,0 +1,5 @@
+age=int(input("enter age: ")) #it takes age input from user
+if age >= 18: #our condition
+    print("Congo!!! You can vote") #result printing
+else:
+    print("Oops!!! You can not vote") #result print
